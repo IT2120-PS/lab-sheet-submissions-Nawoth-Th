7e968cfdabae467cb182b8252daed8545218b492
Nawōth Dissānayake <nawoth.th@gmail.com>
2025-09-02 14:06:06 +0530

    Add Lab 6 Exercises to Github

---
+++ Lab Sheet 06/IT24102280 - Lab Sheet 06.R
@@ -0,0 +1,13 @@
+setwd("C:\\Users\\it24102280\\Documents\\IT24102280")
+getwd()
+
+#1)
+#i. Binomial distribution.
+#ii.
+1-pbinom (46,50,0.85, lower.tail=TRUE)
+
+#2)
+#1. Number of calls per hour.
+#ii. Poisson distribution.
+#iii.
+dpois (15,12)
